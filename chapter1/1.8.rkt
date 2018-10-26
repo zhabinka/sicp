d649e9e1c8fc40e78375b0d34a48eb18054b841c
zhabinka <sergeykarpuk@yandex.by>
2018-10-27 02:35:48 +0300

    Used block structure

--- chapter1/1.8.rkt
+++ chapter1/1.8.rkt
@@ -4,20 +4,17 @@
 (require rackunit)
 
 (define (cube-root x)
+	(define (good-enough? current previous)
+		(< (abs (/ (- current previous) current)) 0.0000001))
+	(define (improve guess)
+		(average (/ x (square guess)) (* 2 guess) 3))
+	(define (cube-root-iter guess guess-previous)
+		(if (good-enough? guess guess-previous)
+				guess
+				(cube-root-iter (improve guess) guess)))
     (if (= x 0)
         0
-	    (cube-root-iter 1.0 0.1 x)))
-
-(define (cube-root-iter guess guess-previous x)
-	(if (good-enough? guess guess-previous)
-			guess
-			(cube-root-iter (improve guess x) guess x)))
-
-(define (improve guess x)
-	(average (/ x (square guess)) (* 2 guess) 3))
-
-(define (good-enough? current previous)
-	(< (abs (/ (- current previous) current)) 0.0000001))
+	    (cube-root-iter 1.0 0.1)))
 
 (check-equal? (cube-root (cube 156)) 156.0 "integer")
 (check-equal? (cube-root (cube 7465.876)) 7465.876 "float")
